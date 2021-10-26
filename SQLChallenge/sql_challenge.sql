8e1c097b7c4735908fe0cb49f98cb6cf5b25108c
Clifford18 <cliffordjohn.cj@gmail.com>
2021-10-26 12:54:32 +0300

    update SQLChallenge/sql_challenge.sql file

--- SQLChallenge/sql_challenge.sql
+++ SQLChallenge/sql_challenge.sql
@@ -4,17 +4,17 @@ DROP DATABASE IF EXISTS `car_management_system`;
 CREATE DATABASE `car_management_system`; 
 USE `car_management_system`;
 
- 
- DROP TABLE IF EXISTS `carsales`;
 
- CREATE TABLE `carsales` (
-   `sale_id` 			bigint 			NOT NULL AUTO_INCREMENT,
-   `car_maker`		varchar(30) 	NOT NULL,
-   `car_model` 	varchar(30) 	NOT NULL,
-   `sale_price_in_dollar` 		int(11) 	NOT NULL,
-   `sale_date` 		timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (`sale_id`)
- ) ;
+    DROP TABLE IF EXISTS `carsales`;
+    CREATE TABLE `carsales` (
+    `sale_id` 			bigint 			NOT NULL AUTO_INCREMENT,
+    `car_maker`		varchar(30) 	NOT NULL,
+    `car_model` 	varchar(30) 	NOT NULL,
+    `sale_price_in_dollar` 		int(11) 	NOT NULL,
+    `sale_date` 		timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+    PRIMARY KEY (`sale_id`)
+    );
 
 INSERT INTO carsales (car_maker, car_model, sale_price_in_dollar, sale_date) 
 		VALUES ('Maker A','A Model X V6',2000,'2021-06-18 15:54:43'),
