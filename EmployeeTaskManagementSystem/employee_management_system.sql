c68d7d346540e08b1efe07e027eebe0e822c08d5
Clifford18 <cliffordjohn.cj@gmail.com>
2021-10-26 12:50:42 +0300

    update EmployeeTaskManagementSystem/employee_management_system.sql

--- EmployeeTaskManagementSystem/employee_management_system.sql
+++ EmployeeTaskManagementSystem/employee_management_system.sql
@@ -1,67 +1,82 @@
+  
+  DROP DATABASE IF EXISTS `employee_management_system`;
+  CREATE DATABASE `employee_management_system`; 
+  
+  USE `employee_management_system`;
 
 
-DROP DATABASE IF EXISTS `employee_management_system`;
-CREATE DATABASE `employee_management_system`; 
-USE `employee_management_system`;
+    DROP TABLE IF EXISTS `employees`;
+    CREATE TABLE `employees` (
+      `employee_id`	bigint	 		NOT NULL AUTO_INCREMENT,
+      `email_id`		varchar(40) 	DEFAULT NULL,
+      `first_name` 	varchar(30) 	DEFAULT NULL,
+      `last_name`		varchar(30) 	DEFAULT NULL,
+      `date_created` 	timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      `date_modified` 	timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+      
+      PRIMARY KEY (`employee_id`),
 
-DROP TABLE IF EXISTS `employees`;
-
-CREATE TABLE `employees` (
-   `employee_id`	bigint	 		NOT NULL AUTO_INCREMENT,
-   `email_id`		varchar(40) 	DEFAULT NULL,
-   `first_name` 	varchar(30) 	DEFAULT NULL,
-   `last_name`		varchar(30) 	DEFAULT NULL,
-   `date_created` 	timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   `date_modified` 	timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-   PRIMARY KEY (`employee_id`),
-   UNIQUE KEY `uindex_employees_email_id` (`email_id`)
- );
- 
- DROP TABLE IF EXISTS `tasks`;
-
- CREATE TABLE `tasks` (
-   `task_id` 			bigint 			NOT NULL AUTO_INCREMENT,
-   `employee_id` 		bigint 			NOT NULL,
-   `task_description`	varchar(50) 	NOT NULL,
-   `task_status` 		varchar(45) 	NOT NULL,
-   `date_created` 		timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   `date_modified` 		timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-   PRIMARY KEY (`task_id`),
-   KEY `task_employees_fk` (`employee_id`),
-   CONSTRAINT `fk_tasks_employees_employee_id` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`) ON DELETE CASCADE ON UPDATE CASCADE
- ) ;
- 
- DELIMITER $$
-CREATE PROCEDURE generate_employees()
-BEGIN
-  DECLARE i INT DEFAULT 1;
-  WHILE i < 10001 DO
-    INSERT INTO `employees` (`email_id`,`first_name`,`last_name`) 
-		VALUES (
-            concat('Email-id - ' ,i,'@gmail.com' ),
-            concat('firstname - ',i ),
-            concat('lastname - ',i )
+      KEY `index_employees_date_created`(`date_created`),
+      KEY `index_employees_date_modified`(`date_modified`),
+      
+      UNIQUE KEY `uindex_employees_email_id` (`email_id`)
     );
-    SET i = i + 1;
-  END WHILE;
-END$$
-DELIMITER ;
-CALL generate_employees();
- 
-DROP PROCEDURE IF EXISTS generate_task;
-DELIMITER $$
-CREATE PROCEDURE generate_task()
-BEGIN
-  DECLARE i INT DEFAULT 1;
-  WHILE i < 500001 DO
-    INSERT INTO `tasks` (`employee_id`,`task_description`,`task_status`) 
-		VALUES (
-			CAST((RAND()*(10000-1)+1) AS SIGNED) ,
-            concat('taskdescription - ',i ),
-            concat('taskstatus - ',i )
-    );
-    SET i = i + 1;
-  END WHILE;
-END$$
-DELIMITER ;
-CALL generate_task();
+
+
+    DROP TABLE IF EXISTS `tasks`;
+    CREATE TABLE `tasks` (
+    `task_id` 			bigint 			NOT NULL AUTO_INCREMENT,
+    `employee_id` 		bigint 			NOT NULL,
+    `task_description`	varchar(50) 	NOT NULL,
+    `task_status` 		varchar(45) 	NOT NULL,
+    `date_created` 		timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `date_modified` 		timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    
+    PRIMARY KEY (`task_id`),
+
+    KEY `index_tasks_date_created`(`date_created`),
+    KEY `index_tasks_date_modified`(`date_modified`),
+
+    UNIQUE KEY `uindex_tasks_task_id_employee_id`(`task_id`,`employee_id`),
+    
+    KEY `fk_employees_tasks__employee_id` (`employee_id`),
+    CONSTRAINT `fk_employees_tasks__employee_id` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`) ON UPDATE CASCADE ON DELETE RESTRICT 
+    ) ;
+
+    DELIMITER $$
+      CREATE PROCEDURE generate_employees()
+        BEGIN
+          DECLARE i INT DEFAULT 1;
+          WHILE i < 10001 DO
+            INSERT INTO `employees` (`email_id`,`first_name`,`last_name`) 
+            VALUES (
+                    concat('Email-id - ' ,i,'@gmail.com' ),
+                    concat('firstname - ',i ),
+                    concat('lastname - ',i )
+            );
+            SET i = i + 1;
+          END WHILE;
+        END$$
+    DELIMITER ;
+
+    CALL generate_employees();
+
+
+    DROP PROCEDURE IF EXISTS generate_task;
+    DELIMITER $$
+      CREATE PROCEDURE generate_task()
+        BEGIN
+          DECLARE i INT DEFAULT 1;
+          WHILE i < 500001 DO
+            INSERT INTO `tasks` (`employee_id`,`task_description`,`task_status`) 
+            VALUES (
+              CAST((RAND()*(10000-1)+1) AS SIGNED) ,
+                    concat('taskdescription - ',i ),
+                    concat('taskstatus - ',i )
+            );
+            SET i = i + 1;
+          END WHILE;
+        END$$
+    DELIMITER ;
+    
+    CALL generate_task();
